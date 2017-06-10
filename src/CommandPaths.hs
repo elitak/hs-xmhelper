c5622d1293239791b53d9cc02cffefead42fd076
Eric Litak <elitak@gmail.com>
2017-06-10 11:53:04 -0700

    rename func

--- src/CommandPaths.hs
+++ src/CommandPaths.hs
@@ -7,15 +7,15 @@ data Command = Rsync
              | TransmissionRemote
   deriving (Eq)
 
-condConvert :: String -> Text
-condConvert str
+shortenIfWasntSubstituted :: String -> Text
+shortenIfWasntSubstituted str
   | (x:xs) <- str, x == '@' = P.last $ splitOn (pack "/") (pack xs)
   | otherwise = pack str
 
 -- NB the substitution variable names must be valid bash names (i.e. no dashes)
 instance Show Command where
-        show Rsync =  unpack . condConvert $ "@rsync@/bin/rsync"
-        show TransmissionRemote =  unpack . condConvert $ "@transmisson@/bin/transmission-remote"
+        show Rsync =  unpack . shortenIfWasntSubstituted $ "@rsync@/bin/rsync"
+        show TransmissionRemote =  unpack . shortenIfWasntSubstituted $ "@transmisson@/bin/transmission-remote"
 
 
 -- TODO
